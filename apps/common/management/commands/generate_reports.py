48d463ee2dd6aebad9c3834b9ade1f703027afa7
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-09-01 19:33:45 -0300

    Add management commands for generating reports, sitemaps, and thumbnails

---
+++ apps/common/management/commands/generate_reports.py
@@ -0,0 +1,8 @@
+﻿from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Run generate_reports'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Running generate_reports...')
---
+++ apps/common/management/commands/generate_sitemaps.py
@@ -0,0 +1,8 @@
+﻿from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Run generate_sitemaps'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Running generate_sitemaps...')
