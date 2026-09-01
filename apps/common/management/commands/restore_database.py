c57009ba640b5ac21daddc314adb7c8f0100cbc4
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-09-01 19:33:59 -0300

    Add management commands for database restoration and maintenance

---
+++ apps/common/management/commands/restore_database.py
@@ -0,0 +1,8 @@
+﻿from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Run restore_database'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Running restore_database...')
---
+++ apps/common/management/commands/run_maintenance.py
@@ -0,0 +1,8 @@
+﻿from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Run run_maintenance'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Running run_maintenance...')
