614779073729a6a311b167e1d32324a0737ca246
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-09-01 19:29:35 -0300

    Add management commands for database backup, analytics calculation, and stock level checking

---
+++ apps/common/management/commands/backup_database.py
@@ -0,0 +1,8 @@
+﻿from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Run backup_database'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Running backup_database...')
---
+++ apps/common/management/commands/check_stock_levels.py
@@ -0,0 +1,8 @@
+﻿from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Run check_stock_levels'
+
+    def handle(self, *args, **options):
+        self.stdout.write('Running check_stock_levels...')
