5571bae3a62549022c467a3075cd30f496406e10
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-09-15 17:58:22 -0300

    Add factories for Cart and Coupon models

---
+++ tests/factories/cart_factory.py
@@ -0,0 +1,7 @@
+﻿import factory
+from apps.cart.models import Cart
+
+
+class CartFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Cart
---
+++ tests/factories/coupon_factory.py
@@ -0,0 +1,9 @@
+﻿import factory
+from apps.marketing.models import Coupon
+
+
+class CouponFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Coupon
+    code = factory.Faker('bothify', text='####-????')
+    discount_value = 10
