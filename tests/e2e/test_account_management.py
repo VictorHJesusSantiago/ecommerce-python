dc6e1323a5880b2a8857e5fca586b0417d2b1d01
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-09-15 17:57:59 -0300

    Add initial test files for account and cart management

---
+++ tests/e2e/test_account_management.py
@@ -0,0 +1,6 @@
+﻿import pytest
+
+
+class Testtest_account_management:
+    def test_placeholder(self):
+        pass
---
+++ tests/e2e/test_cart_management.py
@@ -0,0 +1,6 @@
+﻿import pytest
+
+
+class Testtest_cart_management:
+    def test_placeholder(self):
+        pass
