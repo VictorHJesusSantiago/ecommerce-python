58baeb1b0ab011f7291b86fbe790e04842e40b99
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-09-15 17:58:35 -0300

    Add factories for Review and User models, and initialize fixtures

---
+++ tests/factories/review_factory.py
@@ -0,0 +1,9 @@
+﻿import factory
+from apps.reviews.models import Review
+
+
+class ReviewFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Review
+    rating = 5
+    body = factory.Faker('paragraph')
---
+++ tests/factories/user_factory.py
@@ -0,0 +1,9 @@
+﻿import factory
+from apps.users.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+    email = factory.Faker('email')
+    username = factory.Faker('user_name')
