70b2663509c56c4a1e0d5058d17eb13581b4716b
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-09-15 18:12:58 -0300

    Add initial integration test files for product and user registration flows

---
+++ tests/integration/test_product_management_flow.py
@@ -0,0 +1,6 @@
+﻿import pytest
+
+
+class Testtest_product_management_flow:
+    def test_placeholder(self):
+        pass
---
+++ tests/integration/test_user_registration_flow.py
@@ -0,0 +1,6 @@
+﻿import pytest
+
+
+class Testtest_user_registration_flow:
+    def test_placeholder(self):
+        pass
