daea5dd2ae30d9276cf387e6588d0c8d1a75ca97
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-09-15 17:58:29 -0300

    Add factories for Order and Product models

---
+++ tests/factories/order_factory.py
@@ -0,0 +1,8 @@
+﻿import factory
+from apps.orders.models import Order
+
+
+class OrderFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Order
+    status = 'pending'
---
+++ tests/factories/product_factory.py
@@ -0,0 +1,10 @@
+﻿import factory
+from apps.products.models import Product
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Product
+    name = factory.Faker('sentence', nb_words=3)
+    slug = factory.Faker('slug')
+    price = factory.Faker('pydecimal', left_digits=3, right_digits=2, positive=True)
