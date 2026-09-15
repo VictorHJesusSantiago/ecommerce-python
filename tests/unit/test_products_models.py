5649cdbf18eb20eb2a6e8d75690ed510be7eacbe
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-09-15 18:19:17 -0300

    Add initial unit test files for products models, serializers, and tasks with placeholder tests

---
+++ tests/unit/test_products_models.py
@@ -0,0 +1,6 @@
+﻿import pytest
+
+
+class TestProductsModels:
+    def test_placeholder(self):
+        pass
---
+++ tests/unit/test_products_tasks.py
@@ -0,0 +1,6 @@
+﻿import pytest
+
+
+class TestProductsTasks:
+    def test_placeholder(self):
+        pass
